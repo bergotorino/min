73fce7b2893b0e97cf5aef633ab09319cfddbd81
j4b0l <j4b0l@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-02-20 12:43:48 +0000

    Corrected errors in documentation images and new build script
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@89 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

--- doc/texinfo/build.sh
+++ doc/texinfo/build.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 
-tex min.texi
-tex min-reference.texi
-texindex min.??
-texindex min-reference.??
-tex min.texi
-tex min-reference.texi
-texindex min.??
-texindex min-reference.??
-tex min.texi
-tex min-reference.texi
-
-pdftex min.texi
-pdftex min-reference.texi
-
-texi2html min.texi
-texi2html min-reference.texi
-
-makeinfo min.texi
-makeinfo min-reference.texi
+case "$1" in
+	"pdf")
+	pdftex min.texi
+	pdftex min-reference.texi
+	;;
+	"docbook")
+	makeinfo --docbook min.texi
+	makeinfo --docbook min-reference.texi
+	;;
+	"info")
+	makeinfo min.texi
+	makeinfo min-reference.texi
+	;;
+	"plaintext")
+	makeinfo --plaintext --no-ifinfo min.texi > min.txt
+	makeinfo --plaintext --no-ifinfo min-reference.texi >min-reference.txt
+	;;
+	"html-s")
+	makeinfo --html min.texi
+	cp -r images min/
+	makeinfo --html min-reference.texi
+	cp -r images min-reference/
+	;;
+	#"html-c")
+	#texi2html --node-files -split chapter -prefix min -top-file index.htm -toc-file contents.htm min.texi
+	#ln -s images min/images
+	#ln -s images min-reference/images
+	#;;
+	"html")
+	makeinfo --no-split --html min.texi
+	makeinfo --no-split --html min-reference.texi
+	;;
+	*)
+	echo "USAGE: $0 <format>"
+	echo "  Available formats:"
+	echo "  pdf       - PDF files"
+	echo "  docbook   - docbook XML files"
+	echo "  info      - .info files"
+	echo "  plaintext - plain text .txt files"
+	#echo "  html-c    - HTML multi-file documentation,"
+	#echo "              splitted to chapters"
+	echo "  html-s    - HTML multi-file documentation,"
+	echo "              splitted to nodes"
+	echo "  html      - HTML single-file documentation"
+	;;
+esac
 
 
