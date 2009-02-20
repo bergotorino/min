db2522dfbd3c99033ec3304e04c333447fdfcfef
j4b0l <j4b0l@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-02-20 08:20:22 +0000

    Updates in userguide on 'complete' keyword fix. Removing unnecessary file from documentation.
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@86 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

--- doc/texinfo/chapters/mail_replacement.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find /home/maemo/documentation/texinfo/chapters|grep -v svn|grep -v pdf |grep texi | while read y; do
- sed "s/"/\"/g" $y > /tmp/temp_mj_chap.txt; cat /tmp/temp_mj_chap.txt > $y;
-done
