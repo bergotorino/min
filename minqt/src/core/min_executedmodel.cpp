702b8e09230182bbd4eb42b31a3c89486e3361e8
j4b0l <j4b0l@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-04-30 12:32:51 +0000

    model for executed test cases
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@313 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

---
+++ minqt/src/core/min_executedmodel.cpp
@@ -0,0 +1,81 @@
+/*`
+ * This file is part of MIN Test Framework. Copyright © 2008 Nokia Corporation
+ * and/or its subsidiary(-ies).
+ * Contact: Konrad Marek Zapalowicz
+ * Contact e-mail: DG.MIN-Support@nokia.com
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, version 2 of the License. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General  Public License for
+ * more details. You should have received a copy of the GNU General Public 
+ * License along with this program. If not,  see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
+
+// System includes
+#include <QSize>
+
+// Module include
+#include "min_executedmodel.hpp"
+
+// -----------------------------------------------------------------------------
+Min::ExecutedModel::ExecutedModel(QObject *parent)
+    : QAbstractTableModel(parent)
+    , db_(Min::Database::getInstance())
+{ ; }
+// -----------------------------------------------------------------------------
+Min::ExecutedModel::~ExecutedModel()
+{ ; }
+// -----------------------------------------------------------------------------
+int Min::ExecutedModel::rowCount(const QModelIndex &parent) const
+{
+    return 10;
+}
+// -----------------------------------------------------------------------------
+int Min::ExecutedModel::columnCount(const QModelIndex &parent) const
+{
+    return 3;
+}
+// -----------------------------------------------------------------------------
+QVariant Min::ExecutedModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) return QVariant();
+
+    int row = index.row();
+    int column = index.column();
+
+    if (role==Qt::DisplayRole) {
+        // Device_id is id from database not the one supplied by MIN
+	    QVector<QStringList> data = db_.getExecutedView(1);
+
+        if (row < data.count() && column < 3 ) {
+            return data[row][column];
+        } else return "Nothing";
+    }
+    return QVariant();
+}
+// -----------------------------------------------------------------------------
+QVariant Min::ExecutedModel::headerData(int section,
+                                        Qt::Orientation orientation,
+                                        int role) const
+{
+    if (orientation==Qt::Horizontal) {
+        if (role==Qt::DisplayRole ) {
+            if (section==0) return "Module Name";
+            if (section==1) return "Test Case Name";
+            if (section==2) return "Description";
+            if (section==3) return "Tags";
+        }
+        return QVariant();
+    } else {
+        if (role!=Qt::DisplayRole) return QVariant();
+        return section+1;
+    }
+}
+// -----------------------------------------------------------------------------
+// file created by generator.sh v1.08
---
+++ minqt/src/core/min_executedmodel.hpp
@@ -0,0 +1,76 @@
+/*
+ * This file is part of MIN Test Framework. Copyright © 2008 Nokia Corporation
+ * and/or its subsidiary(-ies).
+ * Contact: Konrad Marek Zapalowicz
+ * Contact e-mail: DG.MIN-Support@nokia.com
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, version 2 of the License. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General  Public License for
+ * more details. You should have received a copy of the GNU General Public 
+ * License along with this program. If not,  see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef INCLUDED_MIN_EXECUTEDMODEL_HPP
+#define INCLUDED_MIN_EXECUTEDMODEL_HPP
+
+// System includes
+#include <QAbstractTableModel>
+
+// Min includes
+#include "min_database.hpp"
+
+// -----------------------------------------------------------------------------
+/**
+ * @namespace Min
+ * @brief
+ */
+namespace Min
+{
+    // -------------------------------------------------------------------------
+    /**
+     * @class ExecutedModel
+     * @brief
+     */
+    class ExecutedModel: public QAbstractTableModel
+    {
+    Q_OBJECT
+    public:
+        /** Default C++ Constructor. */
+        ExecutedModel(QObject *parent);
+
+        /** Destructor. */
+        ~ExecutedModel();
+    
+    public:
+
+        /**@{ Inherited from QAbstractTableModel */
+        int rowCount(const QModelIndex &parent=QModelIndex()) const;
+
+        int columnCount(const QModelIndex &parent=QModelIndex()) const;
+
+        QVariant data(const QModelIndex &index, int role=Qt::DisplayRole) const;
+
+        QVariant headerData(int section,
+                            Qt::Orientation orientation,
+                            int role=Qt::DisplayRole) const;
+        /**@}*/
+
+    private:
+        /** Make it noncopyable */
+        Q_DISABLE_COPY(ExecutedModel)
+
+        /** Handler to database connection for MIN */
+        Min::Database &db_;
+    };
+    // -------------------------------------------------------------------------
+}; // namespace Min
+// -----------------------------------------------------------------------------
+#endif // INCLUDED_MIN_EXECUTEDMODEL_HPP (file created by generator.sh v1.08)
+
