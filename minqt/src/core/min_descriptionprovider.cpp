210148794bf854af6bc301dc72837d02403ad3cd
j4b0l <j4b0l@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-05-08 07:55:49 +0000

    test case status and result description provider
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@375 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

---
+++ minqt/src/core/min_descriptionprovider.cpp
@@ -0,0 +1,94 @@
+/*
+ * This file is part of MIN Test Framework. Copyright © 2008 Nokia Corporation
+ * and/or its subsidiary(-ies).
+ * Contact: Maciej Jablonski
+ * Contact e-mail: DG.MIN-Support@nokia.com
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, version 2 of the License. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General  Public License for
+ * more details. You should have received a copy of the GNU General Public 
+ * License along with this program. If not,  see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
+
+
+
+
+#include "min_descriptionprovider.hpp"
+#include "tmc_common.h"
+#include "min_common.h"
+
+// ----------------------------------------------------------------------------
+Min::DescriptionProvider::DescriptionProvider()
+{
+	/*EMPTY BODY */
+};
+
+// ----------------------------------------------------------------------------
+Min::DescriptionProvider::~DescriptionProvider()
+{
+	/* EMPTY BODY */
+};
+// ----------------------------------------------------------------------------
+QString Min::DescriptionProvider::getTestCaseStatusDescription(int tcstatus)
+{
+	switch(tcstatus){
+		case TP_NONE:
+			return QString("None");
+			break;
+		case TP_RUNNING:
+			return QString("Running");
+			break;
+		case TP_PAUSED:
+			return QString("Paused");
+			break;
+		case TP_ENDED:
+			return QString("Ended");
+			break;
+		case TP_TIMEOUT:
+			return QString("Timeout");
+			break;
+		case TP_ABORTED:
+			return QString("Aborted");
+			break;
+		case TP_CANCELED:
+			return QString("Canceled");
+			break;
+		default:
+			return QString("Unknown");
+	}
+};
+// ----------------------------------------------------------------------------
+QString Min::DescriptionProvider::getTestCaseResultDescription(int tcresult)
+{
+	switch(tcresult){
+		case TP_CRASHED:
+			return QString("Crashed");
+			break;
+		case TP_TIMEOUTED:
+			return QString("Timeouted");
+			break;
+		case TP_PASSED:
+			return QString("Passed");
+			break;
+		case TP_FAILED:
+			return QString("Failed");
+			break;
+		case TP_NC:
+			return QString("Not completed");
+			break;
+		case TP_LEAVE:
+			return QString("Leaved");
+			break;
+		default:
+			return QString("Unknown");
+	}
+
+};
+// ----------------------------------------------------------------------------
---
+++ minqt/src/core/min_descriptionprovider.hpp
@@ -0,0 +1,60 @@
+/*
+ * This file is part of MIN Test Framework. Copyright © 2008 Nokia Corporation
+ * and/or its subsidiary(-ies).
+ * Contact: Maciej Jablonski
+ * Contact e-mail: DG.MIN-Support@nokia.com
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, version 2 of the License. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General  Public License for
+ * more details. You should have received a copy of the GNU General Public 
+ * License along with this program. If not,  see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef INCLUDED_MIN_DESCRIPTIONPROVIDER_HPP
+#define INCLUDED_MIN_DESCRIPTIONPROVIDER_HPP
+
+// System includes
+#include <QVariant>
+
+// Min includes
+
+// -----------------------------------------------------------------------------
+/**
+ * @namespace Min
+ * @brief
+ */
+namespace Min
+{
+    // ---------------------------------------------------------------------
+    class DescriptionProvider: public QObject
+    {
+    Q_OBJECT
+    public:
+	/**
+	 * default destructor
+	 */
+        DescriptionProvider();
+        ~DescriptionProvider();
+
+	static QString getTestCaseStatusDescription(int tcstatus);
+
+	static QString getTestCaseResultDescription(int tcresult);
+
+
+    signals:
+
+    private:
+    };
+    // -------------------------------------------------------------------------
+};
+// namespace Min
+// -----------------------------------------------------------------------------
+#endif  // INCLUDED_MIN_DESCRIPTIONPROVIDER
+
