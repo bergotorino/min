d7e5e1d1487e6809cda485cbf6925fd2fba8841b
samposa <samposa@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-05-11 15:35:13 +0000

    regexp
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@402 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

--- minqt/src/ui/min_executedtab.cpp
+++ minqt/src/ui/min_executedtab.cpp
@@ -61,7 +61,7 @@ Min::ExecutedTab::ExecutedTab(QWidget *parent)
     abortedProxy_->setSourceModel(executedCasesModel_);
     abortedProxy_->setFilterKeyColumn(6);
     //    abortedProxy_->setFilterWildcard(Min::DescriptionProvider::getTestCaseResultDescription(TP_CRASHED));
-    abortedProxy_->setFilterRegExp("[TP_NC|TP_CRASHED]");
+    abortedProxy_->setFilterRegExp("[\"Crashed\"|\"Not completed\"]");
 
     // Prints
     printMsgModel_->setStringList(db_.getPrintoutView(1));
