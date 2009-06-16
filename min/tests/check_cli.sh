3507049f100d8cbe2de600bd4b7ee56ccd819909
samposa <samposa@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-06-16 11:28:46 +0000

    cli checks
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@470 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

---
+++ min/tests/check_cli.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+failures=0
+
+min -h
+failures=`expr $failures + $?`
+min --help
+failures=`expr $failures + $?`
+min -v
+failures=`expr $failures + $?`
+min --version
+failures=`expr $failures + $?`
+min -ct "Demo_1"
+failures=`expr $failures + $?`
+min -console --title Demo_1
+failures=`expr $failures + $?`
+min --console --execute /usr/lib/min/minDemoModule.so
+failures=`expr $failures + $?`
+min -cx /usr/lib/min/minDemoModule.so
+failures=`expr $failures + $?`
+min -cr Demo
+failures=`expr $failures + $?`
+min -c -t Demo_1 --slave localhost:phone
+failures=`expr $failures + $?`
+
+test $failures -eq 0
