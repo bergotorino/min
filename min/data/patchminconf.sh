e50e76883f409be0c4c1ebcf2f8b130eb5064d76
samposa <samposa@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-09-09 06:58:06 +0000

    added support for patching also test case files
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@634 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

--- min/data/patchminconf.sh
+++ min/data/patchminconf.sh
@@ -2,18 +2,39 @@
 set -x
 # $1 config file
 # $2 module
+# $3 test case file
 CONFFILE="$1"
 CONFSTR="ModuleName=$2"
+TESTFILE="TestCaseFile=$3"
 
 if [ ! -e $CONFFILE ] ; then
    cp /etc/min.conf $CONFFILE
 fi
 
-grep $CONFSTR $CONFFILE
-if [ $? -ne 0 ] ; then
+if [ $# -lt 2 ] ; then
+    return 1;
+fi
+
+
+if [ $# -eq 2 ] ; then
+    grep $CONFSTR $CONFFILE
+    if [ $? -eq 0 ] ; then
+	return 0;
+    fi
     cp $CONFFILE $CONFFILE.backup
     echo "[New_Module]" >> $CONFFILE;
     echo $CONFSTR  >> $CONFFILE;
     echo "[End_Module]" >> $CONFFILE;
 fi
 
+if [ $# -eq 3 ] ; then
+    grep $TESTFILE $CONFFILE
+    if [ $? -eq 0 ] ; then
+	return 0;
+    fi
+    cp $CONFFILE $CONFFILE.backup
+    echo "[New_Module]" >> $CONFFILE;
+    echo $CONFSTR  >> $CONFFILE;
+    echo $TESTFILE  >> $CONFFILE;
+    echo "[End_Module]" >> $CONFFILE;
+fi
