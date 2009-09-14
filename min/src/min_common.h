36cd8ae061d217b240143370dc427053a08e31fd
samposa <samposa@86ee150f-fb1f-4fea-b4f8-59f22ff7da1d>
2009-09-14 10:19:42 +0000

    version++
    git-svn-id: https://vcs.maemo.org/svn/min/trunk@643 86ee150f-fb1f-4fea-b4f8-59f22ff7da1d

--- min/src/min_common.h
+++ min/src/min_common.h
@@ -42,7 +42,7 @@
 /** No error definition */
 #define ENOERR 0
 /** MIN version */
-#define MIN_VERSION_STR "min2009w36"
+#define MIN_VERSION_STR "min2009w38"
 /* ------------------------------------------------------------------------- */
 /** Max length of the Test Result description.
  */
